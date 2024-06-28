deaa79bc34244cb6f6667e638035e7e6bec8085e
RainbowKey <113303649+Rainb0wKey@users.noreply.github.com>
2024-06-28 14:31:54 +0300

    $upperCase

---
+++ src/MZscript/Functions/Text/uppercase.py
@@ -0,0 +1,19 @@
+import disnake
+
+from ...functions_handler import FunctionsHandler
+
+
+class Functions(FunctionsHandler):
+    def __init__(self, handler):
+        super().__init__()
+        self.handler = handler
+        self.bot = handler.client.bot
+
+    # [text]
+    async def func_uppercase(self, ctx: disnake.message.Message, args: str):
+        args_list = await self.get_args(await self.is_have_functions(args, ctx))
+        
+        return args_list[0].upper()
+        
+def setup(handler):
+    return Functions(handler)
