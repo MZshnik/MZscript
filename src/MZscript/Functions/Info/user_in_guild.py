c21f675a77474551b64d56a3dcf03656ff1ee206
Falkys <dmvmanea@gmail.com>
2024-06-28 10:35:08 +0300

    $userInGuild Errors update and add to README

--- src/MZscript/Functions/Info/user_in_guild.py
+++ src/MZscript/Functions/Info/user_in_guild.py
@@ -11,11 +11,22 @@ class UserInGuild(FunctionsHandler):
 
     async def func_useringuild(self, ctx: disnake.message.Message, args: str):
         args_list = await self.get_args(await self.is_have_functions(args, ctx), ctx)
-
+        
         if len(args_list) < 1 or len(args_list) == 0:
-            raise ValueError("$roleInfo: To few or no args provided")
-        guild = self.bot.get_guild(ctx.guild.id)
+            raise ValueError("$userInGuild: To few or no args provided")
+        if not args_list[0].isdigit():
+            raise SyntaxError(f"$userInGuild: User id: \"{args_list[0]}\", not int")
+        if len(args_list) >= 2:
+            if not args_list[1].isdigit():
+                raise SyntaxError(f"$userInGuild: Guild id: \"{args_list[1]}\", not int")
+            guild = self.bot.get_guild(int(await self.is_have_functions(args_list[1], ctx)))
+            if not guild:
+                raise SyntaxError(f"$userInGuild: Cannot find guild \"{await self.is_have_functions(args_list[1], ctx)}\"")
+        else:
+            guild = self.bot.get_guild(ctx.guild.id)
+        
         user = guild.get_member(int(await self.is_have_functions(args_list[0], ctx)))
+        
         if user:
             return "true"
         else:
